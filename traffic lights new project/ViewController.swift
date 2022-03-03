02f703d0ad1e4e9e2c36db664cb7162432599a62
Eugenia <pyssiki@yandex.ru>
2022-03-03 18:58:20 +0500

    new code and realization

--- traffic lights new project/ViewController.swift
+++ traffic lights new project/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var buttonColorView: UIButton!
+    @IBOutlet weak var greenLightView: UIView!
+    @IBOutlet weak var yellowLightView: UIView!
+    @IBOutlet weak var redLightView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        
     }
-
-
+    
+    let defaultAlphaLabel: CGFloat = 0.3
+    let startAlphaLabel: CGFloat = 1
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        redLightView.layer.cornerRadius = redLightView.frame.height / 2
+        yellowLightView.layer.cornerRadius = yellowLightView.frame.height / 2
+        greenLightView.layer.cornerRadius = greenLightView.frame.height / 2
+    
+    }
+    
+    
+    @IBAction func buttonColorViewPressed() {
+        buttonColorView.setTitle("NEXT", for: .normal)
+        if redLightView.alpha == startAlphaLabel  {
+            redLightView.alpha = defaultAlphaLabel
+            yellowLightView.alpha = startAlphaLabel
+        } else if yellowLightView.alpha == startAlphaLabel {
+            yellowLightView.alpha = defaultAlphaLabel
+            greenLightView.alpha = startAlphaLabel
+        } else if greenLightView.alpha == startAlphaLabel {
+            greenLightView.alpha = defaultAlphaLabel
+            redLightView.alpha = startAlphaLabel
+        } else {
+            redLightView.alpha = startAlphaLabel
+        }
+    }
+    
 }
 
